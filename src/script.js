367bd6e31023c29fcdaa5827c6bbf62eaa4db4a4
GuillaumeFlambard <guillaume.flambard01@gmail.com>
2020-03-04 14:27:47 +0100

    start to add gandi api call

--- src/script.js
+++ src/script.js
@@ -15,10 +15,19 @@ var app = new Vue({
             })
         },
         checkAvalabilities: function () {
-            this.ovhCall();
-            // namecheap
-            // gandi
+            // this.ovhCall();
+            // this.namecheapCall();
+            this.gandiCall();
             // routes 53
+            // GoDaddy (attention ils proposent de sites)
+            // Bluehost
+            // Enom
+            // 1and1
+            // domain.com
+            // dreamHost
+            // name.com
+            // ipage
+            // A2 Hosting
 
         },
         ovhCall: function () {
@@ -29,6 +38,22 @@ var app = new Vue({
                     console.log('Prix final', response.body[0].prices[4].price.value);
                     this.pushDomainHost('OVH', response.body[0].prices[0].price.value);
             });
+        },
+        namecheapCall: function () {
+
+        },
+        gandiCall: function () {
+            // 4hpdouxdIIA9NCAm0GoC4ay9
+            this.$http.get('https://api.gandi.net/v5/domain/check?name=' + this.domaineName, {headers: {"authorization": "Apikey 4hpdouxdIIA9NCAm0GoC4ay9"}}).then(function(response) {
+                    console.log('Gandi response', response);
+            });
+
+//             INCLUDED WITH EVERY DOMAIN NAME
+
+// 2 mailboxes with 3GB storage
+// Unlimited aliases and forwarding
+// 1 Standard SSL certificate 1 Year to activate
+// Our LiveDNS nameservers Anycast + DNSSEC
         }
     }
 });
